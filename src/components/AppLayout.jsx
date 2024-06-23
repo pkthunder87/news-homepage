52590c9d8f4bf5a0afab6f9c23a6ab4237f39686
pkthunder87 <pkthunder87@gmail.com>
2024-06-22 17:28:32 -0700

    Feat: Add header/logo for desktop design

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -5,7 +5,7 @@ import TopArticles from './TopArticles';
 
 function AppLayout() {
   return (
-    <div className="grid h-[200dvh] w-[100dvw] grid-cols-1 px-24 py-12 md:h-[100dvh] md:grid-cols-[70%_30%] md:grid-rows-[15%_60%_25%]">
+    <div className="grid h-[200dvh] w-[100dvw] grid-cols-1 px-24 py-12 md:h-[100dvh] md:grid-cols-[68%_32%] md:grid-rows-[15%_60%_25%]">
       <Header />
       <Article />
       <Sidebar />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,26 @@
 function Header() {
-  return <div className="bg-blue-500 md:col-span-2">Nav Links</div>;
+  return (
+    <div className="col-span-2 flex items-center justify-between">
+      <img className="h-6" src="logo.svg" alt="logo" />
+      <li className="flex list-none gap-5 text-[.67rem] leading-4">
+        <ul className="cursor-pointer text-neutral-dark-grayish-blue hover:text-primary-soft-red">
+          Home
+        </ul>
+        <ul className="cursor-pointer text-neutral-dark-grayish-blue hover:text-primary-soft-red">
+          New
+        </ul>
+        <ul className="cursor-pointer text-neutral-dark-grayish-blue hover:text-primary-soft-red">
+          Popular
+        </ul>
+        <ul className="cursor-pointer text-neutral-dark-grayish-blue hover:text-primary-soft-red">
+          Trending
+        </ul>
+        <ul className="cursor-pointer text-neutral-dark-grayish-blue hover:text-primary-soft-red">
+          Categories
+        </ul>
+      </li>
+    </div>
+  );
 }
 
 export default Header;
