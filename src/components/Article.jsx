b141ee46e56ccb67a826909e2d43d9689920dda8
pkthunder87 <pkthunder87@gmail.com>
2024-06-29 04:00:35 -0700

    Feat: Finish mobile design for Article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -7,16 +7,16 @@ function Article() {
       </picture>
 
       <div className="grid md:grid-cols-[52.2%_47.8%]">
-        <h1 className="mt-7 text-[2.5rem] font-extrabold text-neutral-very-dark-blue md:mt-[1.35rem] md:text-[2.15rem] md:leading-[2.18rem]">
+        <h1 className="mt-6 text-[2.5rem] font-extrabold leading-[1em] text-neutral-very-dark-blue md:mt-[1.35rem] md:text-[2.15rem] md:leading-[2.18rem]">
           The Bright Future of Web 3.0?
         </h1>
         <div className="flex h-full flex-col justify-between">
-          <p className="mt-[1.21rem] text-[.58rem] leading-4 text-neutral-dark-grayish-blue">
+          <p className="mt-4 text-[.92rem] leading-[1.6rem] text-neutral-dark-grayish-blue md:mt-[1.21rem] md:text-[.58rem] md:leading-4">
             We dive into the next evolution of the web that claims to put the
             power of the platforms back into the hands of the people. But is it
             really fulfilling its promise?
           </p>
-          <button className="-mb-1 h-[1.88rem] w-[7.1rem] bg-primary-soft-red text-[.55rem] font-bold tracking-[.3em] text-neutral-very-dark-blue hover:bg-neutral-very-dark-blue hover:text-white">
+          <button className="mb-5 mt-[1.62rem] h-12 w-[11.5rem] bg-primary-soft-red pt-[.15rem] text-sm font-bold tracking-[.3em] text-neutral-very-dark-blue hover:bg-neutral-very-dark-blue hover:text-white md:-mb-1 md:mt-0 md:h-[1.88rem] md:w-[7.1rem] md:text-[.55rem]">
             READ MORE
           </button>
         </div>
