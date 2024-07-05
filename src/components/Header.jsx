e0a0f7f0c7ef37963d232560193c6e6e2e6fb3a4
pkthunder87 <pkthunder87@gmail.com>
2024-07-05 06:34:48 -0700

    Feat: Add menu open close functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 function Header() {
   const [isMobile, setIsMobile] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
 
   useEffect(() => {
     if (window.innerWidth < 750) {
@@ -20,11 +20,23 @@ function Header() {
         <img className="h-7 md:h-6" src="logo.svg" alt="logo" />
       </h1>
       {isMobile ? (
-        <img
-          className="h-[1.1rem] hover:cursor-pointer"
-          src="icon-menu.svg"
-          alt="logo"
-        />
+        <div>
+          {isMenuOpen ? (
+            <img
+              className="-mt-[.1rem] mr-1 h-[1.9rem] hover:cursor-pointer"
+              src="icon-menu-close.svg"
+              alt="logo"
+              onClick={() => setIsMenuOpen(() => !isMenuOpen)}
+            />
+          ) : (
+            <img
+              className="h-[1.1rem] hover:cursor-pointer"
+              src="icon-menu.svg"
+              alt="logo"
+              onClick={() => setIsMenuOpen(() => !isMenuOpen)}
+            />
+          )}
+        </div>
       ) : (
         <li className="flex list-none gap-[1.6rem] text-[.6rem] leading-4 tracking-tight">
           <ul className="cursor-pointer text-neutral-dark-grayish-blue hover:text-primary-soft-red">
